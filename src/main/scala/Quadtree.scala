27a4120995e038552c8acba1e76661d6cfc0e07a
AlviseDeFaveri <alvise.defaveri@skywarder.eu>
2019-12-08 03:52:39 +0100

    Finish basic requirements - Implement average colors of childs - Implement the toImg() function for quadtree - Implement rotation, mirroring and color inversion - Implement compression - Everything also works with rectangular images

--- src/main/scala/Quadtree.scala
+++ src/main/scala/Quadtree.scala
@@ -1,7 +1,6 @@
 import java.awt.Color
 import java.awt.image.BufferedImage
 import scala.collection.mutable.ListBuffer
-import scala.reflect.ClassTag
 
 import Rgb.RgbBitmap
 
@@ -12,23 +11,29 @@ import Rgb.RgbBitmap
  */
 class Quadtree(val img: RgbBitmap) {
     var color: Color = new Color(0,0,0)
+    var width: Int = img.width
+    var height: Int = img.height
     var children: ListBuffer[Quadtree] = new ListBuffer[Quadtree]()
 
-    println("Node created " + img.width + " " + img.height)
+    //println("Node created " + img.width + " " + img.height)
 
     if(img.hasSameColor || (img.height * img.width) < 1) {
         color = img.matrix(0)(0)
     } else {
-        // split in 4 sub-images
+        // split into 4 sub-images
         val subImgs = splitImg(img)
-        subImgs.foreach(x => println(x.size + " " +  x(0).size))
 
-        // add 4 children to the list
+        // add children to the list
         subImgs.foreach( x => {
-            children += new Quadtree(new RgbBitmap(x))
+            children += new Quadtree(x)
         })
 
-        // color = average(children.color)
+        if (subImgs.size != 2 && subImgs.size != 4)
+            System.exit(-1*subImgs.size)
+
+        // Take the mean color of the children
+        val sum = children.map(_.color).map(x => List(x.getRed(), x.getGreen(), x.getBlue())).reduceLeft((x,y) => {x.zip(y).map(a => a._1 + a._2)})
+        color = new Color(sum(0)/children.size, sum(1)/children.size, sum(2)/children.size)
     }
 
     /** Is this node a leaf? */
@@ -36,15 +41,74 @@ class Quadtree(val img: RgbBitmap) {
 
     /** Print name of the Node and subnodes in a tree */
     def show(prefix: String) {
-        print(prefix + "_ ")
-        print(color)
-        print('\n')
+        println(prefix + "_ " + width + "x" + height)
         children.foreach(_.show(prefix + "  |"))
     }
 
     /** Split an image into 4 sub images */
-    private def splitImg(img: RgbBitmap): List[List[List[Color]]] = {
-        img.matrix.map(_.grouped((img.width+1)/2).toList).grouped((img.width+1)/2).map(_.transpose).reduce(_ ++ _)
+    private def splitImg(img: RgbBitmap): List[RgbBitmap] = {
+        img.matrix.map(_.grouped((img.height+1)/2).toList).grouped((img.width+1)/2).map(_.transpose).reduce(_ ++ _).map(new RgbBitmap(_))
+    }
+
+    /** convert quadtree to image */
+    //@annotation.tailrec
+    final def toBitmap(depth: Int): RgbBitmap = {
+
+        // If it's a leaf or we reached the wanted detail, return the color
+        if(this.isLeaf  || depth <= 0){
+            val mat = List.fill(width*height)(color).grouped(width).toList.transpose
+            return new RgbBitmap(mat)
+        }
+
+        // Else...
+        else {
+            // recursively get the children's bitmap
+            val submaps: List[List[List[Color]]] = children.map(_.toBitmap(depth - 1).matrix).toList
+
+            /* Case 4 children
+             *  +---+---+
+             *  | 0 | 1 |
+             *  +---+---+
+             *  | 2 | 3 |
+             *  +---+---+
+             */
+            if(submaps.size > 2)
+            {
+                val img = (submaps(0), submaps(1)).zipped.map(_ ++ _) ::: (submaps(2), submaps(3)).zipped.map(_ ++ _)
+                return new RgbBitmap(img);
+            }
+            // Case 2 Children
+            else {
+
+                /* Vertical
+                 *  +---+
+                 *  | 0 |
+                 *  +---+
+                 *  | 1 |
+                 *  +---+
+                 */
+                if(height == 1) {
+                    val img = submaps(0) ::: submaps(1)
+                    return new RgbBitmap(img);
+                }
+
+                /* Horizontal
+                 *  +---+---+
+                 *  | 0 | 1 |
+                 *  +---+---+
+                 */
+                else if(width==1) {
+                    val img = (submaps(0), submaps(1)).zipped.map(_ ++ _)
+                    return new RgbBitmap(img);
+                }
+
+                else {
+                    return new RgbBitmap(List(List(color)))
+                }
+            }
+
+        }
+
     }
 
 }
